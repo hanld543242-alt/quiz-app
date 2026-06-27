61600dbc7f42f4cc5a2f4faabdc0989c0c7ab8c9
Duy1509-kd <duygaming12345kh@gmail.com>
2026-06-27 10:36:22 +0700

    Update data_loader.h

--- data_loader.h
+++ data_loader.h
@@ -1,31 +1,29 @@
-#pragma once 
+#pragma once
 
 #include <iostream>
 #include <vector>
 #include <string>
 
-using namespace std; 
-
 class Question {
 private:
     int id;                           
-    string question_text;        
-    vector<string> options;      
+    std::string question_text;        
+    std::vector<std::string> options; 
     char correct_answer;              
 
 public:
     Question() : id(0), correct_answer(' ') {}
-    Question(int id, const string& text, const vector<string>& opts, char answer) 
+    Question(int id, const std::string& text, const std::vector<std::string>& opts, char answer)
         : id(id), question_text(text), options(opts), correct_answer(answer) {}
 
     int getId() const { return id; }
-    string getQuestionText() const { return question_text; } 
-    vector<string> getOptions() const { return options; }    
+    std::string getQuestionText() const { return question_text; }
+    std::vector<std::string> getOptions() const { return options; }
     char getCorrectAnswer() const { return correct_answer; }
 };
 
 class DataLoader {
 public:
-    static vector<Question> loadData(const string& filename);         
-    static void printQuestions(const vector<Question>& questions); 
+    static std::vector<Question> loadData(const std::string& filename);
+    static void printQuestions(const std::vector<Question>& questions);
 };
